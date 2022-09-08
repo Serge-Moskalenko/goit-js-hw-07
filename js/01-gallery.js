bd8794567f985429e5fc5bc528c82c66dbb289ea
Serge-Moskalenko <skmos2019@outlook.com>
2022-09-08 17:06:07 +0100

    finish

---
+++ js/01-gallery.js
@@ -0,0 +1,47 @@
+import { galleryItems } from './gallery-items.js';
+
+const refs = {
+    divEl:document.querySelector('.gallery')
+}
+
+function createMarkup(e) {
+    return e.map(ev => {
+
+        return `<div class="gallery__item">
+  <a class="gallery__link" href="${ev.original}">
+    <img
+      class="gallery__image"
+      src="${ev.preview}"
+      data-source="${ev.original}"
+      alt='${ev.description}'
+    />
+  </a>
+</div>`
+    }).join('');
+};
+
+const markup = createMarkup(galleryItems);
+
+refs.divEl.insertAdjacentHTML('beforeend', markup);
+
+refs.divEl.addEventListener('click', onClick);
+
+function onClick(e) {
+    e.preventDefault();
+    if (e.target.nodeName !== 'IMG') {
+        return;
+    };
+    const instance = basicLightbox.create(`
+    
+    <img src=${e.target.dataset.source}>`
+    );
+    
+    instance.show()
+    window.addEventListener('keydown', e => {
+        if (e.code === 'Escape') {
+        instance.close()
+    }})
+};
+
+
+
---
+++ js/02-lightbox.js
@@ -0,0 +1,30 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+const refs = {
+    divEl:document.querySelector('.gallery')
+}
+
+function createMarkup(e) {
+    return e.map(ev => {
+
+        return `
+  <a class="gallery__item" href="${ev.original}">
+    <img
+      class="gallery__image"
+      src="${ev.preview}"
+      data-source="${ev.original}"
+      alt='${ev.description}'
+    />
+  </a>
+</div>`
+    }).join('');
+};
+
+const markup = createMarkup(galleryItems);
+
+refs.divEl.insertAdjacentHTML('beforeend', markup);
+
+var lightbox = new SimpleLightbox('.gallery .gallery__item', {captionsData:'alt',captionDelay:250});
+
+
